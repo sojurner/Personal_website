4c848f4abddd3bedb9eece8a06f4506fef239137
sojurner <pykim0591@gmail.com>
2018-12-21 13:00:01 -0700

    Work

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -11,13 +11,23 @@ export const Work = () => {
         <i className="fab fa-confluence" />
         My Work
       </h1>
-      <Carousel>
+      <Carousel interval={null}>
         {projectsData.map(project => {
+          const { gif_path, github_url, live_url, project_name } = project;
           return (
             <Carousel.Item>
-              <img src={project.gif_path} />
+              <img src={gif_path} />
               <Carousel.Caption>
-                <h3>{project.project_name}</h3>
+                <h3>{project_name}</h3>
+                <i
+                  onClick={() => window.open(github_url, '_blank')}
+                  className="fab fa-github"
+                />
+                <i
+                  target="_blank"
+                  onClick={() => window.open(live_url, '_blank')}
+                  className="fas fa-code-branch"
+                />
               </Carousel.Caption>
             </Carousel.Item>
           );
